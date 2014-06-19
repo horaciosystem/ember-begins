cb62478523047843d4b25a0b434ad35d6f915f87
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2014-06-18 21:57:12 -0300

    implemented partial views

--- js/app.js
+++ js/app.js
@@ -7,28 +7,40 @@ App.Router.map(function() {
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return Ember.$.getJSON('http://localhost:3000/polls.json');
-  },
+  }
+});
 
+App.IndexController = Ember.ArrayController.extend({
+	currentPartial: "poll_question",
   actions: {
-    selectItem: function(poll){
-    	this.selectedItem = poll;
-    },
-
     doVote: function() {
+    	var controller = this;
     	var poll = this.selectedItem;
       if(!poll) return alert('Selecione uma opção!')
 
-      Ember.$.ajax({method: 'PUT', url: 'http://localhost:3000/polls/' + poll.id + '/vote', headers: { 'Content-type': 'application/json'}})
-    	.success(function(polls, status, headers, config) {
-          model = polls;  
-          alert('Obrigado por votar!');
-      })
-      .error(function(data, status, headers, config) {
-          alert('Ops :( Houve um erro durante a votação! Tente mais tarde.');
-      });   
+      Ember.$.ajax({type: 'PUT', url: 'http://localhost:3000/polls/' + poll.id + '/vote', headers: { 'Content-type': 'application/json'}})
+	    	.success(function(polls, status, xhr) {
+    		  controller.set("model", polls);
+    		  controller.set("currentPartial", "poll_result");
+    		  alert('Obrigado por votar!');
+	      })
+	      .error(function(data, status, xhr) {
+	        alert('Ops :( Houve um erro durante a votação! Tente mais tarde.');
+	      });   
     }
   }
 });
 
+App.RadioButton = Ember.Component.extend({
+    tagName : "input",
+    type : "radio",
+    attributeBindings : [ "name", "type", "value", "checked:checked" ],
+    click : function() {
+        this.set("selection", this.get("value"));
+    },
+    checked : function() {
+        return this.get("value") === this.get("selection");
+    }.property('selection')
+});
 
-
+Em.Handlebars.helper('radio-button',App.RadioButton);
